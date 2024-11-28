f992f4b89ed9a9c630d764fac8f9539decf4f448
Parth Shah <pshah947795@gmail.com>
2024-11-28 11:51:34 +0530

    feat: allow new lines as delimiters

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,7 +3,9 @@ class StringCalculator {
         if (!numbers) return 0; // Return 0 if input is empty
     
         // Split by default comma delimiter
-        const numberList = numbers.split(",").map((num) => parseInt(num, 10));
+        const delimiterRegex = /[,\n]/; // Regex for comma or new line
+        const numberList = numbers.split(delimiterRegex).map((num) => parseInt(num, 10));
+
     
         return numberList.reduce((sum, num) => sum + num, 0); // Sum all numbers
     }
--- src/__tests__/StringCalculator.test.js
+++ src/__tests__/StringCalculator.test.js
@@ -20,6 +20,11 @@ describe("StringCalculator", () => {
         expect(calculator.add("1,2")).toBe(3);
         expect(calculator.add("1,2,3,4")).toBe(10);
     });
+
+    test("should handle new lines as delimiters", () => {
+        expect(calculator.add("1\n2,3")).toBe(6);
+        expect(calculator.add("1\n2\n3")).toBe(6);
+    });
     
     
 });
